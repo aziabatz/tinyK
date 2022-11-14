a1bdafb370d2bd700ca84b0147f95ff3cc2855cd
aziabatz <aziabatz@alumnos.unex.es>
2022-11-14 01:00:17 +0100

    IDT and PIC documentation. Documentación de IDT y PIC

--- include/cpu/int/idt.h
+++ include/cpu/int/idt.h
@@ -44,39 +44,82 @@
 #include <cpu/desc/descriptor.h>
 #include <cpu/regs.h>
 
+/// \brief Número máximo de líneas de interrupción de la tabla de interrupciones
 #define MAX_IDT_ENTRIES 256
 
+/// \brief Bandera para interrupciones de tarea
 #define IDT_GATE_TASK 0x5
+/// \brief Bandera para interrupciones con selectores de 16 bits
 #define IDT_GATE_INT16 0x6
+/// \brief Bandera para interrupciones software con selectores de 16 bits
 #define IDT_GATE_TRAP16 0x07
+/// \brief Bandera para interrupciones con selectores de 16 bits
 #define IDT_GATE_INT32 0x0E
+/// \brief Bandera para interrupciones con selectores de 32 bits
 #define IDT_GATE_TRAP32 0x0F
 
+/// \brief Macro para asignar bits del anillo de privilegios de la interrupción
 #define IDT_DPL(dpl) (dpl<<5)
+/// \brief Bit que indica que la interrupción está presente en la tabla
 #define IDT_P (1 << 7)
 
 #ifndef KERNEL_CS
+/// \brief Selector por defecto del segmento de código del núcleo.
 #define KERNEL_CS 0x08
 #endif
 
+/**
+ * \brief Descriptor de puerta de interrupción en la tabla
+ * 
+ */
 struct idt_gate_desc
 {
+    /// \brief Parte baja(16 bits) de la dirección del manejador de interrupción
     uint16 offset_low;
+    /// \brief Selector de segmento donde se ejecuta la interrupción
     uint16 seg_sel;
+    /// \brief Bits reservados
     uint8 reserved;
+    /// \brief Atributos de la puerta de interrupción
     uint8 flags;
+    /// \brief Parte alta(16 bits) de la dirección del manejador de interrupción
     uint16 offset_high;
 }__attribute__((packed));
 
+/// \brief Tipo registro de descriptor de interrupción
 typedef struct idt_gate_desc idt_gate_t;
+/// \brief Tipo de tabla de desciptores como: Tabla de descriptores de interrupción
 typedef descriptor_t idt_t;
 
+/**
+ * \brief Función para poner a punto la tabla de descriptores de interrupción y cargarla al registro correspondiente IDTR
+ * 
+ */
 void idt_init();
 
+/**
+ * \brief Función que inserta una interrupción en la tabla
+ * 
+ * \param entry Número de entrada en la tabla de interrupciones
+ * \param offset Dirección del manejador de interrupción
+ * \param selector Selector del segmento de código donde se ejecuta la interrupción
+ * \param flags Atributos de la interrupción en la tabla
+ */
 void idt_set_gate(uint32 entry, uint32 offset, uint16 selector, uint8 flags);
 
+/**
+ * \brief Asigna un nuevo manejador a una interrupción en la tabla
+ * 
+ * \param entry Número de entrada en la tabla de interrupciones
+ * \param handler Dirección del nuevo manejador de interrupción
+ */
 void idt_set_handler(uint32 entry, uint32 handler);
 
+/**
+ * \brief Manejador último común a todos las interrupciones
+ * 
+ * \param regs Marco de registros del procesador en el instante de la interrupción
+ */
 void isr_handler(reg_frame_t * regs);
 
 #endif
--- include/cpu/int/pic.h
+++ include/cpu/int/pic.h
@@ -1,27 +1,28 @@
-/*
- * Created: Wednesday, October 12th 2022, 6:00:22 pm
- * Author: Ahmed Ziabat Ziabat
+/**
+ * \file pic.h
+ * \date Wednesday, October 12th 2022, 6:00:22 pm
+ * \author Ahmed Ziabat Ziabat
  * 
  * 
  * BSD 3-Clause License
  * 
- * Copyright (c) 2022, Ahmed Ziabat Ziabat <aziabatz@alumnos.unex.es>
+ * \copyright Copyright (c) 2022, Ahmed Ziabat Ziabat <aziabatz@alumnos.unex.es>
  * All rights reserved.
- * 
+ * * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ * * 
  * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- * 
+ * list of conditions and the following disclaimer.
+ * * 
  * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- * 
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * * 
  * 3. Neither the name of the copyright holder nor the names of its
- *    contributors may be used to endorse or promote products derived from
- *    this software without specific prior written permission.
- * 
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -32,38 +33,79 @@
  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * \brief Fichero de cabecera para el manejo del controlador PIC 8259
  */
+
 #ifndef TK_PIC_H
 #define TK_PIC_H
 
 #include <stdbool.h>
 #include <dev/io/port.h>
 
+/// \brief Puerto del controlador maestro
 #define PIC_MASTER          0x20
+/// \brief Puerto E/S para comando del controlador maestro
 #define PIC_MASTER_COMMAND  PIC_MASTER
+/// \brief Puerto E/S para datos del controlador maestro
 #define PIC_MASTER_DATA     PIC_MASTER+1
 
+/// \brief Puerto del controlador esclavo
 #define PIC_SLAVE           0xA0
+/// \brief Puerto E/S para comando del controlador esclavo
 #define PIC_SLAVE_COMMAND   PIC_SLAVE
+/// \brief Puerto E/S para datos del controlador esclavo
 #define PIC_SLAVE_DATA      PIC_SLAVE+1
 
+/// \brief Palabra de comando de inicialización del controlador
 #define PIC_ICW4            0x01
+/// \brief Ĉomando de inicialización
 #define PIC_INIT            0x10
 
+/// \brief Número de líneas de interrupción del controlador maestro
 #define PIC_MASTER_LINES    8
+/// \brief Número de líneas de interrupción del controlador esclavo
 #define PIC_SLAVE_LINES     8
+/// \brief Número máximo de líneas de ambos controladores en cascada
 #define PIC_MAX_LINES       PIC_MASTER_LINES + PIC_SLAVE_LINES
 
+/// \brief Desplazamiento dentro de la tabla de interrupciones para interrupciones provienientes del PIC
 #define PIC_OFFSET          PIC_MASTER
 
+/**
+ * \brief Activa o desactiva el controlador aplicando la máscara correspondiente
+ * 
+ * \param enable Nuevo estado activado/desactivado del controlador
+ */
 void pic_set(bool enable);
 
+/**
+ * \brief Devuelve el estado de activación del controlador
+ * 
+ * \return true Si el controlador PIC está activo
+ * \return false En caso contrario
+ */
 bool pic_is_enabled();
 
+/**
+ * \brief Reasigna las interrupciones provenientes del PIC con respecto a `#PIC_OFFSET`
+ * 
+ */
 void pic_remap();
 
+/**
+ * \brief Reasigna las interrupciones provenientes del PIC con respecto a un desplazamiento dado
+ * 
+ * \param master Nuevo desplazamiento de las interrupciones del maestro
+ * \param slave Desplazamiento de las interrupciones del esclavo
+ */
 void pic_remap_offset(uint8 master, uint8 slave);
 
+/**
+ * \brief Comunica a los controladores el reconocimiento de una solicitud de interrupción recibida
+ * 
+ * \param ack_slave Indica si se debe enviar el reconocimiento de interrupción a ambos controladores
+ */
 void pic_ack(bool ack_slave);
 
 #endif
